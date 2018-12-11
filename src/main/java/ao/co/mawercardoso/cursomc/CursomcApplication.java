a038d615d2cc01821d23985349624335f908ae44
Mawer Cardoso <mawerc@gmail.com>
2018-12-12 00:35:28 +0100

    Projecto Criado

---
+++ src/main/java/ao/co/mawercardoso/cursomc/CursomcApplication.java
@@ -0,0 +1,12 @@
+package ao.co.mawercardoso.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+}
