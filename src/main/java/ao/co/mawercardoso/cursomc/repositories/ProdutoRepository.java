ee56573ab4d5be8b463949a3144e710747fd387b
Mawer Cardoso <mawerc@gmail.com>
2018-12-12 02:57:46 +0100

    Produto e associação muitos para muitos

---
+++ src/main/java/ao/co/mawercardoso/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package ao.co.mawercardoso.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ao.co.mawercardoso.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
